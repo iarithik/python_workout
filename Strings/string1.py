0d8db693fc9175e2f10e6462262aa9ece0f38fad
ShivaniDM <Shivanidattaram20@gmail.com>
2024-10-15 20:07:18 +0530

    Modified the code for pig latin

--- Strings/string1.py
+++ Strings/string1.py
@@ -1,11 +1,19 @@
 def pig_latin():
     word = input(" Please enter the word to be translated to pig latin ")
+    if word.isalpha():
+        output = translate_to_pig(word)
+        print(f"Ding ding your results are ready! The translation of {word} in pig latin is {output}")
+    else:
+        print(f"Please include only alphabets")
+
+
+def translate_to_pig(word):
     vowels ="aeiou"
     output=""
-    if (word[0] in vowels):
+    if (word[0].lower() in vowels):
             output = word+"way"
     else:
-        output = word[1:] + word[0]+"ay"
-    print(f"Ding ding your results are ready! The translation of {word} in pig latin is {output}")
-
+        output = word[1: ] + word[0].lower()+"ay"
+    return output
 pig_latin()
+
