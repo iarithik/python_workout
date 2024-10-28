855a6d623da481841a3c189ada372897805da452
ShivaniDM <Shivanidattaram20@gmail.com>
2024-10-29 01:17:50 +0530

    Doing exercise 4

---
+++ ListTuples/LT4.py
@@ -0,0 +1,23 @@
+def most_repeating_word(*str_ip):
+    count_el =0
+    op_string =""
+    for element in str_ip:
+        cur_val = count_str(element)
+        if  cur_val> count_el:
+            count_el = cur_val
+            op_string = element
+    print(f"The word with most repeated letters is {op_string}")
+    
+
+def count_str(string):
+    str_dict ={}
+    count = 1
+    for i in string:
+        if i in str_dict:
+            str_dict[i] = str_dict.get(i)+1
+        else:
+            str_dict.update({i:count})
+    max_count = max(str_dict.values())
+    return max_count
+
+most_repeating_word('this', 'is', 'an', 'elementary', 'test', 'example')
