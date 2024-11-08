ec8dc9ac1196da797b6e3b90a987cb5a0aadf93d
ShivaniDM <Shivanidattaram20@gmail.com>
2024-11-08 23:34:28 +0530

    solved exercises in dict and set 2

--- DS/DS2.py
+++ DS/DS2.py
@@ -1,23 +1,30 @@
-def get_rainfall():
-    city_dict ={}
+city_dict ={}
 
-    check = input("If you are ready to share information press any alphabet :")
-    if bool(check) == False:
+def get_rainfall():
+
+    check = input("If you are ready to share information press 'y' : ")
+    if not check:
         print("See you later !")
-    while bool(check): 
+    while check: 
         city_ip = input("Please enter a city name : ")
-        if city_ip == '':
-            if bool(city_dict) == 0:
-                print("No data available ")
-            else:
-                for key, value in city_dict.items():
-                    print(f"{key}: {value}")
+        if not city_ip:
+            fetch_rainfall()
             break
         else:
-            rainfall_ip = int(input("Please enter rainfall for the city"))
-            if city_ip in city_dict:
-                city_dict[city_ip] = city_dict.get(city_ip) + rainfall_ip
-            else:
-                city_dict.update({city_ip : rainfall_ip})
+            rainfall_ip = int(input("Please enter rainfall for the city  :"))
+            set_rainfall(city_ip,rainfall_ip)
             
+def set_rainfall(city,rainfall):
+    if city in city_dict:
+        city_dict[city] = city_dict.get(city) + rainfall
+    else:
+        city_dict.update({city : rainfall})
+
+def fetch_rainfall():
+    if not city_dict :
+        print("No data available ")
+    else:
+        for key, value in city_dict.items():
+            print(f"{key}: {value}")
+
 get_rainfall()
