5a4144354450c759e5ddee5c97606b392a27c546
ShivaniDM <Shivanidattaram20@gmail.com>
2024-12-04 21:59:10 +0530

    Learned writing to file and directory handling in python

---
+++ Files/F7.py
@@ -0,0 +1,25 @@
+def reverse_file(ip_file,op_file):
+    try :
+        with open(ip_file,'r') as ip:
+            with open(op_file,'w') as op:
+
+                for current_line in ip:
+                    words = current_line.strip().split()
+                    print(words)
+                    for word in reversed(words):
+                        op.write(word[::-1]+" ")
+                    op.write('\n')
+
+    except FileNotFoundError:
+        print(f"Given input file {ip_file} doesn't exist. Please enter correct file")
+    except Exception as e:
+        print(f"Please take a look at the error {e}")
+
+    return op_file
+
+if __name__ =='__main__':
+    ip_file = 'D:/Hottie/Files/ex24IP.txt'
+    op_file = 'D:/Hottie/Files/ex24op.txt'
+
+    op = reverse_file(ip_file,op_file)
+    print(f' process content can be found at {op}')
