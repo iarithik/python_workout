488d90ddab7605c8cafb5cd3699f729c8901f192
ShivaniDM <Shivanidattaram20@gmail.com>
2025-01-08 18:12:44 +0530

    Iterators in python generator functions

--- Iterators/Practice.py
+++ Iterators/Practice.py
@@ -19,11 +19,17 @@ def time_counter():
 
 o = time_counter()
 print(o)
-'''
+
 import time
-
-start = time.perf_counter_ns()
-# Your code to measure
-end = time.perf_counter_ns()
-
-print(f"Elapsed time: {end - start} seconds")
+def elapsed_since(data):
+        last_time = None 
+        for item in data:
+                current_time = time.perf_counter() 
+                delta = current_time - (last_time
+                or current_time) 
+                last_time = time.perf_counter()
+                yield (delta, item) 
+for t in elapsed_since('abcd'):
+        print(t)
+        time.sleep(2)
+'''
